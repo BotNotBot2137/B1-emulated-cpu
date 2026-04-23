b0e2ced0bdf61e6bec5f601e098cb19e11207e93
BotNotBot2137 <no.one.asket@gmail.com>
2026-04-23 21:25:30 +0200

    Update B1.cpp
    Added multi-threading functionality with a new thread that prints a message. Program now exits on specific input.

--- B1.cpp
+++ B1.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <thread>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::thread;
+using std::exit;
+using std::endl;
+
+void T1(){
+    cout << "Hello multi-threading! ff to exit" << endl;
+}
 
 int main(){
-    std::cout << "Hello";
+    string Terminal;
+    thread t1 (T1);
+    cout << "";
+    cin >> Terminal;
+    t1.join();
+    if ("ff" == Terminal){
+        exit(255);
+    }
     return 0;
-}
